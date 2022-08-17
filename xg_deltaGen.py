a190e60e9883a1096f5c2954b1edb32b935ef023
skassekert <skassekert@nvidia.com>
2022-08-17 14:30:54 -0700

    weekly sync

--- xg_deltaGen.py
+++ xg_deltaGen.py
@@ -1,16 +1,97 @@
 """this reads toml data, xgd template and outputs to an xgd for given groomnames"""
-
+import os
+import re
+import numpy as np
+from glob import glob
 import tomli
 from itertools import product
-from xg_tools import deltaOutPath, get_desc, get_grooms, fit
-from glob import glob
 
 
 # pathing
 sourcePath = "G:/Shared drives/TriplegangersGroom_ext/Groom_INTERNAL/"
 configPath = "0000_base_delta/template/"
 configFile = "delta_c000.toml"
+xgPath = "/maya/base/scenes/base__head_coll.xgen"
+maPath = "/maya/base/scenes/base.ma"
+deltaPath = "maya/base_delta/scenes/deltaGen/"
 
+
+def dc_extract(groomName):
+    """parses ma file looking for collections and descriptions.  returns dictionary"""
+    sourcePath = "G:/Shared drives/TriplegangersGroom_ext/Groom_INTERNAL/{}/maya/base/scenes/".format(groomName)
+    xgPath = sourcePath+"/base__head.xgen"
+    maPath = sourcePath+"/base.ma"
+    descPattern = r'[A-Za-z]*Desc\b'
+    collPattern = r'base__.*.xgen'
+
+    def descOut(filename,  pattern):
+        s = None
+        with open(filename, 'r') as f:
+            s = f.read()#.splitlines()
+        return list(set(re.findall(pattern, s)))
+
+    collections = {}
+    if os.path.exists(maPath):
+        for colls in descOut(maPath, collPattern):
+            collections[colls] = descOut(sourcePath+colls, descPattern)
+        return collections
+
+
+def gather_palettes(paths:list="", discard:list=""):
+    """ iterate thru all grooms and create sets of coll(s) and desc(s) and discard(s)"""
+    colls = set()
+    descs = set()
+    for groom in paths:
+        palettes = dc_extract(groom)
+        if palettes:
+            # print(palettes)
+            for key in palettes:
+                # print(palettes.keys())
+                colls.add(key)
+                for items in palettes[key]:
+                    descs.add(items)
+    descp = [x for x in descs if not any(word in x for word in discard)]
+    discards = [x for x in descs if any(word in x for word in discard)]
+    return colls, descp, discards
+
+
+def prune(desc:list="", discard:list=["eye", "baby"]):
+    """ with input list, remove discards """
+    descp = [x for x in desc if not any(word in x.lower() for word in discard)]
+    discards = [x for x in desc if any(word in x.lower() for word in discard)]
+    print("retained:", descp), print("discarded:", discards)
+    return descp
+
+
+def get_grooms(path:str=sourcePath, name:str="**"):
+    """ get grooms from source path.  default name = '**' """
+    return [x.replace(os.sep, '/').split('/')[-1] for x in glob(path+name)]
+
+
+
+def fit(min, max, length, decimals):
+    step = (max - min)/length
+    x = [round(x, decimals) for x in np.arange(min, max, step)]
+    return(x)
+
+def deltaOutPath(groomName):
+    outputPath = "{0}{1}/{2}".format(sourcePath, groomName, deltaPath)
+    if not os.path.exists(outputPath):
+        os.makedirs(outputPath)
+        print("created dir:", outputPath)
+    return outputPath
+
+def get_desc(groomName, ):
+    xgenPath =  "{}{}{}".format(sourcePath, groomName, xgPath)
+    desc_names=[]
+    xg_file =  open(xgenPath, 'r')
+    lines = xg_file.readlines()
+    xg_file.close()
+    for i in range(len(lines)):
+        ck = lines[i]
+        if ck.startswith("Description"):
+            desc_names.append(lines[i+1].rpartition("\t")[2].rstrip("\n"))
+    return desc_names
 
 def __load_config():
     """open config file with read + binary"""
@@ -42,6 +123,10 @@ def clean_deltas():
 
 def dh_coil_gen(groomName, count:list = "", radius:list = ""):
     """gets descriptions from groomName and outputs array of xgd from count/radius"""
+    if isinstance(count, int):
+        count = [count]
+    if isinstance(radius, int):
+        radius = [radius]
     palettes = prune(get_desc(groomName))
     permutations = list(product(*[count, radius]))
     for p in permutations:
@@ -54,7 +139,8 @@ def dh_coil_gen(groomName, count:list = "", radius:list = ""):
                 modifier = modifier.replace("<xgenDescName>", desc)
                 modifier = modifier.replace("<count>", str(p[0]))
                 modifier = modifier.replace("<radius>", str(p[1]))
-                f.write(modifier)
+                print(modifier)
+                # f.write(modifier)
                 
 
 def dh_cutClamp_gen(groomName, cutLength:list = "", mask:float=1.0):
@@ -146,15 +232,17 @@ def dh_wind(groomName,
 # for groomName in namelist:
 #     dh_wind(groomName, direction=[2, .2, 1], stiffness=[0.3, .6], constStrength=[2])
 
-for groomName in get_grooms(sourcePath):
-    expList = [
-        dh_exp_gScale(groomName, fit(.1, 5, 10, 1)),
-        # dh_noise_gen(groomName, fit(.1, 5, 10, 1), fit(1, 10, 5, 1)),
-        # dh_cutClamp_gen(groomName, fit(2, 10, 5, 1)),
-        # dh_coil_gen(groomName, fit(1,10,4,1), fit(.1,3,4,1)),
-        # dh_wind(groomName, direction=[2, .2, 1], stiffness=[0.3, .6], constStrength=[2])
-    ]
+# for groomName in get_grooms(sourcePath):
+#     expList = [
+#         dh_exp_gScale(groomName, fit(.1, 5, 10, 1)),
+#         # dh_noise_gen(groomName, fit(.1, 5, 10, 1), fit(1, 10, 5, 1)),
+#         # dh_cutClamp_gen(groomName, fit(2, 10, 5, 1)),
+#         # dh_coil_gen(groomName, fit(1,10,4,1), fit(.1,3,4,1)),
+#         # dh_wind(groomName, direction=[2, .2, 1], stiffness=[0.3, .6], constStrength=[2])
+#     ]
 
+# dh_coil_gen("simonYuen", np.arange(.5, 2.1, .5), np.arange(0, 2.1, .5))
+# dh_coil_gen("simonYuen", 1, 1)
 
 
 
--- xg_tools.py
+++ xg_tools.py
@@ -1,10 +1,7 @@
-import os
-import re
-from glob import glob
 import maya.cmds as mc
 import xgenm as xge
 import xgenm.xgGlobal as xgg
-import numpy as np
+
 
 sourcePath = "G:/Shared drives/TriplegangersGroom_ext/Groom_INTERNAL/"
 xgPath = "/maya/base/scenes/base__head_coll.xgen"
@@ -26,80 +23,3 @@ def xgd_export():
     col = mc.ls("*_coll")[0]
     exportPath = mc.fileDialog2(fm=0)[0]
     xge.createDelta(str(col), str(exportPath))
-
-
-def dc_extract(groomName):
-    """parses ma file looking for collections and descriptions.  returns dictionary"""
-    sourcePath = "G:/Shared drives/TriplegangersGroom_ext/Groom_INTERNAL/{}/maya/base/scenes/".format(groomName)
-    xgPath = sourcePath+"/base__head.xgen"
-    maPath = sourcePath+"/base.ma"
-    descPattern = r'[A-Za-z]*Desc\b'
-    collPattern = r'base__.*.xgen'
-
-    def descOut(filename,  pattern):
-        s = None
-        with open(filename, 'r') as f:
-            s = f.read()#.splitlines()
-        return list(set(re.findall(pattern, s)))
-
-    collections = {}
-    if os.path.exists(maPath):
-        for colls in descOut(maPath, collPattern):
-            collections[colls] = descOut(sourcePath+colls, descPattern)
-        return collections
-
-
-def gather_palettes(paths:list="", discard:list=""):
-    """ iterate thru all grooms and create sets of coll(s) and desc(s) and discard(s)"""
-    colls = set()
-    descs = set()
-    for groom in paths:
-        palettes = dc_extract(groom)
-        if palettes:
-            # print(palettes)
-            for key in palettes:
-                # print(palettes.keys())
-                colls.add(key)
-                for items in palettes[key]:
-                    descs.add(items)
-    descp = [x for x in descs if not any(word in x for word in discard)]
-    discards = [x for x in descs if any(word in x for word in discard)]
-    return colls, descp, discards
-
-
-def prune(desc:list="", discard:list=["eye", "baby"]):
-    """ with input list, remove discards """
-    descp = [x for x in desc if not any(word in x.lower() for word in discard)]
-    discards = [x for x in desc if any(word in x.lower() for word in discard)]
-    print("retained:", descp), print("discarded:", discards)
-    return descp
-
-
-def get_grooms(path:str=sourcePath, name:str="**"):
-    """ get grooms from source path.  default name = '**' """
-    return [x.replace(os.sep, '/').split('/')[-1] for x in glob(path+name)]
-
-
-def fit(min, max, length, decimals):
-    step = (max - min)/length
-    x = [round(x, decimals) for x in np.arange(min, max, step)]
-    return(x)
-
-def deltaOutPath(groomName):
-    outputPath = "{0}{1}/{2}".format(sourcePath, groomName, deltaPath)
-    if not os.path.exists(outputPath):
-        os.makedirs(outputPath)
-        print("created dir:", outputPath)
-    return outputPath
-
-def get_desc(groomName, ):
-    xgenPath =  "{}{}{}".format(sourcePath, groomName, xgPath)
-    desc_names=[]
-    xg_file =  open(xgenPath, 'r')
-    lines = xg_file.readlines()
-    xg_file.close()
-    for i in range(len(lines)):
-        ck = lines[i]
-        if ck.startswith("Description"):
-            desc_names.append(lines[i+1].rpartition("\t")[2].rstrip("\n"))
-    return desc_names
